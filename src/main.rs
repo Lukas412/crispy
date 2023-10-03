d1248100296d4b8cd6f01517c13394ce51c8ebe5
lukas <lukas.weber.4.12@gmail.com>
2023-10-03 17:22:33 +0200

    wip

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,15 @@
+use std::collections::HashMap;
+
 fn main() {
     let program = ();
+}
+
+fn parse_to_programm(programm: &mut Programm, input: &str) {}
+
+fn parse_and_evaluate(programm: &Programm, input: &str) -> GenericValue {}
+
+struct Programm {
+    functions: HashMap<Box<str>, fn(List<GenericValue>) -> GenericValue>,
 }
 
 enum GenericValue<'a> {
@@ -7,14 +17,14 @@ enum GenericValue<'a> {
     Number(u8),
     Text(&'a str),
     List(List<Self>),
-    FunctionCall(FunctionCall<'a, Self>)
+    FunctionCall(FunctionCall<'a, Self>),
+}
+
+struct List<T> {
+    values: Vec<T>,
 }
 
 struct FunctionCall<'a, Arguments> {
     name: &'a str,
-    arguments: List<Arguments>
-}
-
-struct List<T> {
-    values: Vec<T>
+    arguments: List<Arguments>,
 }
